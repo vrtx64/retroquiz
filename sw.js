9fe6904e44f1c29861dd36a54d88d69f79014c70
Aleksey <vrtx64@gmail.com>
2026-08-16 23:03:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,8 @@ const ASSETS = [
   './',
   './index.html',
   './icon.png'
-  // Добавьте сюда пути к вашим CSS или JS файлам, если они есть, например: './style.css'
 ];
 
-// Установка сервис-воркера и кэширование ресурсов
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -15,7 +13,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Активация и удаление старого кэша
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -30,7 +27,6 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Перехват запросов и выдача файлов из кэша, если нет сети
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
