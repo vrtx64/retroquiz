d2e81b3503395831b26d70543895f24ad3431760
Aleksey <vrtx64@gmail.com>
2026-08-16 23:03:00 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'retro-quiz-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.png'
+  // Добавьте сюда пути к вашим CSS или JS файлам, если они есть, например: './style.css'
+];
+
+// Установка сервис-воркера и кэширование ресурсов
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Активация и удаление старого кэша
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов и выдача файлов из кэша, если нет сети
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
